8764c4d0c2221e76ca0d13b88f8d9aa6bdd92c9e
rare1k <hi@rare1k.dev>
2024-04-05 18:38:48 -0400

    feat: launch animation

--- index.js
+++ index.js
@@ -1,9 +1,63 @@
 function onlaunch() {
 document.querySelector("#wait_beforeplay").remove()
 document.querySelector("#beforeplay button#play").addEventListener("click",()=>{
-    document.querySelector("#beforeplay").remove()
+    document.querySelector("#beforeplay").remove();
+    startTitleAnim()
 })
 }
 setTimeout(()=>{
-    onlaunch()
+    onlaunch();
 },250)
+
+function startTitleAnim() {
+    let t = document.createElement("div")
+    
+    t.style.display = "flex"
+    t.style.flexDirection = "row"
+    t.style.justifyContent = "center"
+    t.style.alignItems = "middle"
+    t.style.alignContent = "middle"
+    t.style.width = "100%"
+    t.style.height = "100%"
+    t.innerHTML = `
+    <h1 style="font-weight:bold; font-size:36px;"><span>rare™</span></h1>
+    `
+    let title = t.querySelector("h1")
+    title.style.opacity = "0"
+    title.style.top = "0px"
+    title.style.transition = "all 0.5s"
+    title.style.position = "relative"
+    title.style.textShadow = "4px 24px 33px #fff"
+    document.querySelector("#base").appendChild(t)
+    setTimeout(()=>{
+        title.style.opacity = "1"
+        title.style.top = "45vh"
+        title.style.textShadow = "4px 4px 64px #fff"
+    },300)
+    setTimeout(() => {
+        title.innerHTML += " | "
+        setTimeout(() => {
+            title.innerHTML += "inf"
+        },400)
+        setTimeout(() => {
+            title.innerHTML += "ini"
+        },433)
+        setTimeout(() => {
+            title.innerHTML += "te"
+        },466)
+        setTimeout(() => {
+            title.innerHTML += "pac"
+        },500)
+        setTimeout(() => {
+            title.innerHTML += "er"
+        },520)
+        setTimeout(() => {
+            title.innerHTML += "test"
+        },600)
+        setTimeout(()=>{
+            title.style.fontSize = "16px"
+            title.style.opacity = "0"
+            title.style.textShadow = "0px 0px 0px #000"
+        },1000)
+    },1000)
+}
